ef4198523ca72d13415e62ff00dedde37f1e96b6
Okkar Min <okar@Okkars-MacBook-Pro.local>
2019-03-04 18:20:59 +0800

    meeting 2.5

--- cms/baseapp/urls.py
+++ cms/baseapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     path('map/', views.map, name='map'),
-    path('new_incident/', views.new_incident_form, name='new_incident_form')
+    path('new_incident/', views.new_incident_form, name='new_incident_form'),
+    path('incident_details/', views.incident_details, name='incident_manage'),
 ]
--- cms/baseapp/views.py
+++ cms/baseapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -11,3 +12,6 @@ def map(request):
 
 def new_incident_form(request):
 	return render(request, 'new_incident_form.html', None)
+
+def incident_details(request):
+	return render(request, 'incident_manage.html', None)
