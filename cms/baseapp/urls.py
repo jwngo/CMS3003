be4a3f610bce18b8a7dcac4e3209748221d6e8a5
Okkar <phoehtaungwin@gmail.com>
2019-03-06 12:06:33 +0800

    added login feature

--- cms/baseapp/urls.py
+++ cms/baseapp/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('map/', views.map, name='map'),
     path('new_incident/', views.new_incident_form, name='new_incident_form'),
     path('incident_details/', views.incident_details, name='incident_details'),
