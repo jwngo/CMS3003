2365ad31a47d9633f3901a6ff4843eb54af79ccb
jiawei <jiawei@jiaweis-MacBook-Pro.local>
2019-03-07 18:42:37 +0800

    make dashboard dynamic

--- cms/baseapp/views.py
+++ cms/baseapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import *
 from django.http import HttpResponseRedirect
 import json
 
 # Create your views here.
 
 def dashboard(request):
-	return render(request, 'dashboard.html', None)
+	data = Incident.objects.values()
+	incidents = {'incidents': data}
+	return render(request, 'dashboard.html', incidents)
 
 def incidents_map(request):
 	return render(request, 'map.html', None)
@@ -13,8 +16,11 @@ def incidents_map(request):
 def new_incident_form(request):
 	return render(request, 'new_incident_form.html', None)
 
-def incident_details(request):
-	return render(request, 'incident_details.html', None)
+def incident_details(request, incident_id):
+	data = Report.objects.filter(incident=incident_id)
+	reports = {'reports' : data}
+	print(reports)
+	return render(request, 'incident_details.html', reports)
 
 def manage_incident(request):
 	return render(request, 'manage_incident.html', None)
