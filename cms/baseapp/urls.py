8e345d0b766e486c234f68c1a00388a25b7c15f4
jiawei <jiawei@jiaweis-MacBook-Pro.local>
2019-03-07 18:42:37 +0800

    make dashboard dynamic

--- cms/baseapp/urls.py
+++ cms/baseapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('map/', views.incidents_map, name='map'),
     path('new_incident/', views.new_incident_form, name='new_incident_form'),
-    path('incident_details/', views.incident_details, name='incident_details'),
+    path('incident_details/<int:incident_id>', views.incident_details, name='incident_details'),
     path('manage_incident/', views.manage_incident, name='manage_incident'),
     path('subscribe/', views.subscribe, name='subscribe'),
 ]
