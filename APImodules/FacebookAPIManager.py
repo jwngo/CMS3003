5920b066cf3dd052d4df5d3c9f2fb55880a51a50
jiawei <jiawei@jiaweis-MacBook-Pro.local>
2019-03-07 17:18:12 +0800

    changed Pipfile

--- APImodules/FacebookAPIManager.py
+++ APImodules/FacebookAPIManager.py
@@ -4,12 +4,12 @@ token = {'EAAjjQZAPY4XQBAILMGndeFa93Hfxk31EZAaYzaJt07Rhr6H9BpnWDZAChEYnYtWhDxZBZ
 graph = facebook.GraphAPI(token)
 
 #hardcode, this parameter expecting a string
-info = "This is my testing for fb page"
+info = "I love anime!" 
 
 def facebookShare(info):
     graph.put_object(parent_object='me', connection_name='feed', message=info)
 
     
-
+facebookShare(info)
     
 
