7f0213bafe44f83008bb374645b9bf4838d1d1da
Li Bingzi <LiBingzi_99@icloud.com>
2019-03-07 16:48:42 +0800

    facebook api

---
+++ cms/baseapp/Facebook.py
@@ -0,0 +1,15 @@
+import facebook
+
+token = {'EAAjjQZAPY4XQBAILMGndeFa93Hfxk31EZAaYzaJt07Rhr6H9BpnWDZAChEYnYtWhDxZBZCRdDJm7TspHJMUbEusTlfuuLd61ZBZBWeZBWz3WhAzZBnzGfZAKpsZAXmTr0ycSAAF3cHd3N4r5da99jKeI2xLZAZAZCjs03HJhcHKaKUZBDkzQL6i9FAoVH18HLVoyHOoW2jT7f1EUJ63ngZDZD'}
+graph = facebook.GraphAPI(token)
+
+#hardcode, this parameter expecting a string
+info = "This is my testing for fb page"
+
+def facebookShare(info):
+    graph.put_object(parent_object='me', connection_name='feed', message=info)
+
+    
+
+    
+
