66a98a4fb6433140950a1e35237e3a9a4fbdfd2a
zoeboya <36061695+zoeboya@users.noreply.github.com>
2019-03-22 12:42:30 +0800

    telegram api finished

--- APImodules/TelegramAPIManager.py
+++ APImodules/TelegramAPIManager.py
@@ -1,47 +1,41 @@
 
 '''https://web.telegram.org/#/im?p=s1409214510_613501089696389297
 1409214510
-{
-  "ok": true,
-  "result": {
-    "message_id": 2,
-    "chat": {
-      "id": -1001363941800,
-      "title": "TestChannel",
-      "username": "usefulmix_channel",
-      "type": "channel"
-    },
-    "date": 1538926255,
-    "text": "Test message"
-  }
-}
+procedure to schive this:
+1. create a bot
+2. get its token and ksys
+3. enter https://api.telegram.org/bot798092482:AAEYOmSAs1rL1ejwfWF5ZbkRaTWeQY11jzw
+   as the base url
+5. set this bot as administrator of the channel, private channel_id: -100xxxxxxxxx->in the link of your channel
+4. https://api.telegram.org/bot798092482:AAEYOmSAs1rL1ejwfWF5ZbkRaTWeQY11jzw/sendMessage?text="text_you_want_to_send"&chat_id=-100xxxxxxxxx
+
+
+TIPS:
+1. remember to add bot as administrator
+2. bot api documentation: the methods start with lower letter, 
+   when use, the format is like: /method_name?attribute_name=value
+
+
+'''
+'''
+telegram_post(string: text, string: gphoto_url, string: chat, default)
+'''
+'''
+#test case
+#telegram_post("text_you_want_to_send", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScZ5R7saUPRGc6m7QnAhF0r40MTYMtoELXDyKNuzMSTqydsblb")
 '''
 
 
+import requests
 
-#pip3 install telethon
-from telethon import TelegramClient, events, sync
+def telegram_post(text, photo_url, chat = "-1001475571282"):
+  #this url contained the bot token already
+  url_base = "https://api.telegram.org/bot798092482:AAEYOmSAs1rL1ejwfWF5ZbkRaTWeQY11jzw"
 
-def telegram_post():
-	# These example values won't work. You must get your own api_id and
-	# api_hash from https://my.telegram.org, under API Development.
-	api_id = -1001409214510
-	api_hash = '5c7a824ef6a40a24fe978a46d7ca3a9c'
 
-	client = TelegramClient('session_name', api_id, api_hash)
-	client.start()
+  photo_url = url_base + "/sendPhoto?" + "photo=" + photo_url +"&chat_id=" +chat +"&caption="+text
 
-	print(client.get_me().stringify())
+  requests.post(photo_url)
 
-	client.send_message('username', 'Hello! Talking to you from Telethon')
-	# client.send_file('username', 'http://www.bigredchilli.com/wp-content/uploads/2016/08/Singapore_36x24_colour1_on_blue_etsy1.jpg')
+#telegram_post("text_you_want_to_send", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScZ5R7saUPRGc6m7QnAhF0r40MTYMtoELXDyKNuzMSTqydsblb")
 
-	client.download_profile_photo('me')
-	# messages = client.get_messages('username')
-	# messages[0].download_media()
-
-	@client.on(events.NewMessage(pattern='(?i)hi|hello'))
-	async def handler(event):
-	    await event.respond('Hey!')
-
-telegram_post()
