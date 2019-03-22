4816ca68aebc0555cb89f041684f4752a35c0f53
Okkar Min <okar@Okkars-MacBook-Pro.local>
2019-03-22 12:39:34 +0800

    added firebase.py

--- cms/baseapp/firebase.py
+++ cms/baseapp/firebase.py
@@ -1,19 +1,34 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from .models import *
+
+# Initialize firebase
+cred = credentials.Certificate('./ssadproject-1551665312466-c888723cff4c.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 def saveIncidentToFirebase(request):
   data = request.POST.copy()
   i_type = data.getlist('type_of_incident')
-  num_of_casualties = data.get('num_of_casualties')
-  assitance_requested = data.getlist('assitance_requested')
-  num_ambulance = data.get('num_ambulance_requested')
-  num_firetruck = data.get('num_firetruck_requested')
-  num_police = data.get('num_police_requested')
-  num_gasleak = data.get('num_gasleak_requested')
-  reporter_name = data.get('reporter_name')
-  reporter_number = data.get('reporter_number')
+  i_managedBy = request.user.username
+  i_area = None
+  i_region = None
+  i_status = None
+  i_level = None
+  i_created_at_date = None
+  i_created_at_time = None
   i_address = data.get('i_address')
   i_postalcode = data.get('i_postalcode')
   i_description = data.get('i_description')
-  managedBy = request.user.username
 
-  
+def saveReportToFirebase(i_id, request):
+  data = request.POST.copy()
+  r_num_of_casualties = data.get('num_of_casualties')
+  r_assitance_requested = data.getlist('assitance_requested')
+  r_num_ambulance = data.get('num_ambulance_requested')
+  r_num_firetruck = data.get('num_firetruck_requested')
+  r_num_police = data.get('num_police_requested')
+  r_num_gasleak = data.get('num_gasleak_requested')
+  r_reporter_name = data.get('reporter_name')
+  r_reporter_number = data.get('reporter_number')
