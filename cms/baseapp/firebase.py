597667e3e5f41ddb255f0be6215b8be4bd24d673
Okkar <phoehtaungwin@gmail.com>
2019-03-23 10:42:01 +0800

    rename variables to match with firecloud

--- cms/baseapp/firebase.py
+++ cms/baseapp/firebase.py
@@ -10,25 +10,25 @@ db = firestore.client()
 
 def saveIncidentToFirebase(request):
   data = request.POST.copy()
-  i_type = data.getlist('type_of_incident')
-  i_managedBy = request.user.username
-  i_area = None
-  i_region = None
-  i_status = None
-  i_level = None
-  i_created_at_date = None
-  i_created_at_time = None
-  i_address = data.get('i_address')
-  i_postalcode = data.get('i_postalcode')
-  i_description = data.get('i_description')
+  incident_type = data.getlist('type_of_incident')
+  incident_managedBy = request.user.username
+  incident_area = None
+  incident_region = None
+  incident_status = None
+  incident_level = None
+  incident_created_at_date = None
+  incident_created_at_time = None
+  incident_address = data.get('i_address')
+  incident_postalcode = data.get('i_postalcode')
+  incident_description = data.get('i_description')
 
 def saveReportToFirebase(i_id, request):
   data = request.POST.copy()
-  r_num_of_casualties = data.get('num_of_casualties')
-  r_assitance_requested = data.getlist('assitance_requested')
-  r_num_ambulance = data.get('num_ambulance_requested')
-  r_num_firetruck = data.get('num_firetruck_requested')
-  r_num_police = data.get('num_police_requested')
-  r_num_gasleak = data.get('num_gasleak_requested')
-  r_reporter_name = data.get('reporter_name')
-  r_reporter_number = data.get('reporter_number')
+  report_num_of_casualties = data.get('num_of_casualties')
+  report_assitance_requested = data.getlist('assitance_requested')
+  report_num_ambulance = data.get('num_ambulance_requested')
+  report_num_firetruck = data.get('num_firetruck_requested')
+  report_num_police = data.get('num_police_requested')
+  report_num_gasleak = data.get('num_gasleak_requested')
+  report_reporter_name = data.get('reporter_name')
+  report_reporter_number = data.get('reporter_number')
