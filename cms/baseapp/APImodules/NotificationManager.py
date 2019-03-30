5b97034b3f0fa1066494eac5befeaed2e455559b
zoeboya <36061695+zoeboya@users.noreply.github.com>
2019-03-30 09:29:52 +0800

    1. need to install pillow: pipenv install pillow 2. change few file path 3. NotificationManager.py last line #test for testing

--- cms/baseapp/APImodules/NotificationManager.py
+++ cms/baseapp/APImodules/NotificationManager.py
@@ -1,23 +1,23 @@
-From .FacebookAPIManager import facebookShare
-From .TelegramAPIManager import telegram_post
-From .TwitterAPIManager import twitterShare
-From .FirebaseAPIManager import getIncidentFromFirebase
+from FacebookAPIManager import facebookShare
+from TelegramAPIManager import telegram_post
+# from TwitterAPIManager import twitterShare
+from FirebaseAPIManager import getIncidentFromFirebase
 from PIL import Image
 import requests
 from io import BytesIO
-import datetime import datetime
-
+import datetime 
 ################## hard code data ####################
 # List of CAT2 incident that is posted
 cat2_posted = []
 # time interval between CAT2 posts(senconds): 6hours
-interval1= = 21600
+interval1= 21600
 # civil defense shelter web page url
 url_cd = 'https://www.scdf.gov.sg/home/civil-defence-shelter'
 #image includes information list of hospitals and polyclinics
 url = 'https://firebasestorage.googleapis.com/v0/b/cz2006project.appspot.com/o/hospitalInfo.jpeg?alt=media&token=be53232a-7e7e-450e-a1da-6f035d837750'
 response = requests.get(url)
 image = Image.open(BytesIO(response.content))
+
 
 ################## functions ####################
 #call notify() when sending the incident to firebase
@@ -30,7 +30,7 @@ def notify(incident):
         facebookShare(message, image)
         telegram_post(message, url)
     #For CAT2, update when >= 6 hours from last CAT2 post
-    else if(incident_level == 'CAT2'):
+    elif(incident_level == 'CAT2'):
         check_update(incident_id)
     
 
@@ -54,14 +54,16 @@ def check_update(incident_id):
     
 def get_message(incident):
     types_of_incident = ', '.join(incident['incident_type'])
-	incident_level = incident['incident_level']
-	incident_region = incident['incident_region']
-	incident_address = incident['incident_address']
+    incident_level = incident['incident_level']
+    incident_region = incident['incident_region']
+    incident_address = incident['incident_address']
     
     alert_message = 'Incident Alert! There is a ' + incident_level +' incident in ' + incident_region + ', '+ incident_address +', types of incident:' + types_of_incident + '.'
     shelter_info = 'See civil defense shelter info at: '+url_cd
     hospital_info = 'List of Hospitals and polyclinlcs: see the image below'
-    message = alert_message + '\n' + shelter info + '\n' + hospital_info+'\n'
+    message = alert_message + '\n' + shelter_info + '\n' + hospital_info+'\n'
     
     return message
     
+# boya test
+notify(getIncidentFromFirebase(0))
