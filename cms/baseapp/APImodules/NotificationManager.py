574f2438f8fed87f08f74f5a3b4169ec5bef8e9d
Li-Bingzi <LiBIngzi_99@icloud.com>
2019-04-04 13:31:37 +0800

    NotificationManager dummy data testing passed

--- cms/baseapp/APImodules/NotificationManager.py
+++ cms/baseapp/APImodules/NotificationManager.py
@@ -1,67 +1,96 @@
-From .FacebookAPIManager import facebookShare
-From .TelegramAPIManager import telegram_post
-From .TwitterAPIManager import twitterShare
-From .FirebaseAPIManager import getIncidentFromFirebase
+from FacebookAPIManager import facebookShare
+from TelegramAPIManager import telegram_post
+#from TwitterAPIManager import twitterShare
+from FirebaseAPIManager import getIncidentFromFirebase
 from PIL import Image
 import requests
 from io import BytesIO
-import datetime import datetime
-
+from datetime import datetime
+import time
 ################## hard code data ####################
 # List of CAT2 incident that is posted
 cat2_posted = []
+
 # time interval between CAT2 posts(senconds): 6hours
-interval1= = 21600
+interval = 21600
+
 # civil defense shelter web page url
 url_cd = 'https://www.scdf.gov.sg/home/civil-defence-shelter'
+
+
 #image includes information list of hospitals and polyclinics
 url = 'https://firebasestorage.googleapis.com/v0/b/cz2006project.appspot.com/o/hospitalInfo.jpeg?alt=media&token=be53232a-7e7e-450e-a1da-6f035d837750'
 response = requests.get(url)
 image = Image.open(BytesIO(response.content))
+buf = BytesIO()
+image.save(buf, 'jpeg')
+buf.seek(0)
+image = buf.read()
+buf.close()
+
+shelter_info = 'See civil defense shelter info at: '+url_cd +'\n'
+hospital_info = 'List of Hospitals and polyclinlcs: see the image below\n'
 
 ################## functions ####################
+
 #call notify() when sending the incident to firebase
 def notify(incident):
     incident_id = int(incident['incident_id'])
-    message = get_message(incident)
-    
+    message = get_message(incident) + shelter_info + hospital_info
+    incident_level = incident['incident_level']
     #For CAT1 incident, send immediately
-    if(incident_level == 'CAT1'):
+    if incident_level == 'CAT1':
         facebookShare(message, image)
         telegram_post(message, url)
     #For CAT2, update when >= 6 hours from last CAT2 post
-    else if(incident_level == 'CAT2'):
+    elif incident_level == 'CAT2':
         check_update(incident_id)
     
 
 def check_update(incident_id):
+    #if this is not the first CAT1 incident
     if cat2_posted:
         last_post_id = cat2_posted[-1]
-        last_post_incident = getIncidentFromFirebase(last_post_id)
+        last_post_incident =getIncidentFromFirebase(last_post_id)
         last_post_dateTime = last_post_incident['incident_created_at_date']+last_post_incident['incident_created_at_time']
         datetimeFormat = '%d %B %Y%H:%M'
         current = datetime.now()
-        diff = current - datetime.strptime(last_post_dateTime, dateFormat)
+        diff = current - datetime.strptime(last_post_dateTime, datetimeFormat)
         message = ''
-        if(diff.senconds >= interval):
-            for cid in range(last_posted, incident_id):
+        if diff.seconds >= interval:
+            for cid in range(last_post_id+1, incident_id+1):
                 incident = getIncidentFromFirebase(cid)
-                message += get_message(incident)
-                cat2_posted.append(cid)
+                if incident['incident_level'] == 'CAT2':
+                    message += get_message(incident)
+                    cat2_posted.append(cid)
+            message = message + shelter_info + hospital_info
             facebookShare(message, image)
             telegram_post(message, url)
+    #if this is first CAT2 incident
+    else:
+        incident = getIncidentFromFirebase(incident_id)
+        message = get_message(incident)+ shelter_info + hospital_info
+        cat2_posted.append(incident_id)
+        facebookShare(message, image)
+        telegram_post(message, url)
         
     
 def get_message(incident):
-    types_of_incident = ', '.join(incident['incident_type'])
-	incident_level = incident['incident_level']
-	incident_region = incident['incident_region']
-	incident_address = incident['incident_address']
-    
-    alert_message = 'Incident Alert! There is a ' + incident_level +' incident in ' + incident_region + ', '+ incident_address +', types of incident:' + types_of_incident + '.'
-    shelter_info = 'See civil defense shelter info at: '+url_cd
-    hospital_info = 'List of Hospitals and polyclinlcs: see the image below'
-    message = alert_message + '\n' + shelter info + '\n' + hospital_info+'\n'
-    
-    return message
+    types_of_incident = incident['incident_type']
+    incident_level = incident['incident_level']
+    incident_region = incident['incident_region']
+    incident_address = incident['incident_address']
+    incident_status = incident['incident_status']
+    # different messages for reported and handling, default return empty string(closed). However, when incident is closed, NotificationManager shouldn't be called !
+    if incident_status == 'Reported':
+        message = 'Incident Alert! There is a [possible] '
+    elif incident_status == 'Handling':
+        message = 'We are currently handling a '
+    else:
+        return ''
+    alert_message = message + incident_level +' incident in ' + incident_region + ', '+ incident_address +', types of incident:' + types_of_incident + '.\n'
+    return alert_message
+
+
+
     
